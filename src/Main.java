c58882e147033e56769827f84bd8472925f2a5ab
Ilya201091 <ZiK201091@ya.ru>
2022-08-28 18:13:03 +0700

    первый коммит

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         System.out.println("Hello world!");
 
         int[] weights = new int[] {90, 91, 93, 92, 85, 87, 88, 89, 0, 0 ,0 , 0};
-        weights[0] = 90;
+        int[] weightsCopy = new int[] {902, 91, 93, 92, 85, 87, 88, 89, 0, 0 ,0 , 0};
         int januaryWeights = weights[0];
         System.out.println(januaryWeights);
         System.out.println(weights[0]);
@@ -13,6 +13,32 @@ public class Main {
         for (int i = 0; i < weights.length; i++) {
             System.out.println(weights[i]);
         }
-
+        boolean arraysAreEqual = true;
+        arraysAreEqual = weights.length == weightsCopy.length;
+        if (arraysAreEqual) {
+            for (int i = 0; i < weights.length; i++) {
+                if (weights[i] != weightsCopy[i]) {
+                    arraysAreEqual = false;
+                }
+            }
+        }
+        if (arraysAreEqual) {
+            System.out.println("Массивы одинаковые");
+        } else {
+            System.out.println("Массивы разные");
+        }
+        for (int i = 0; i < weightsCopy.length; i++) {
+            System.out.println(weightsCopy[i]);
+        }
+        int maxWeight = -1;
+        for (final int current : weights) {
+            if (current > maxWeight) {
+                maxWeight = current;
+            }
+        }
+        System.out.println(maxWeight);
+        for (int i = 0; i < weights.length - 1 && weights[i + 1] != 0; i++) {
+            System.out.println(weights[i + 1] - weights[i]);
+        }
     }
 }
